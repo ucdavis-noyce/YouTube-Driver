59555a4a4c47df02a7413acfa7a99cac8e1c377a
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-04 15:49:43 -0700

    added long description

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="YouTube-Driver",
-    version="1.0.0",
+    version="1.0.1",
     packages=["ytdriver"],
     license="MIT",
     author="Muhammad Haroon",
     author_email="mharoon@ucdavis.edu",
     url="https://github.com/ucdavis-noyce/YouTube-Driver",
-    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.0.tar.gz",
+    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.1.tar.gz",
     description="Programmatically interact with YouTube's interface.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     package_dir={"": "src"},
     install_requires=[
         'selenium',
