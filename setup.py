4ac9538d8a0f39695020ed90ca1bdb7629d3cdc6
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-04 15:36:40 -0700

    fixed setup

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 setup(
     name="YouTube-Driver",
     version="0.1",
-    packages=["ytdriver"],
+    packages=["YouTube-Driver"],
     license="MIT",
     author="Muhammad Haroon",
     author_email="mharoon@ucdavis.edu",
     url="https://github.com/ucdavis-noyce/YouTube-Driver",
     download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v0.1.tar.gz",
     description="Programmatically interact with YouTube's interface.",
-    long_description="README.md",
     package_dir={"": "src"},
     install_requires=[
         'selenium',
