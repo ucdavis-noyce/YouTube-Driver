89adf3f89b14a92bc4d05d1da867f2a8d9098d29
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-04 15:32:17 -0700

    added setup.cfg

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ setup(
     name="YouTube-Driver",
     version="0.1",
     packages=["ytdriver"],
+    license="MIT",
     author="Muhammad Haroon",
-    author_email="m_haroon96@hotmail.com",
+    author_email="mharoon@ucdavis.edu",
+    url="https://github.com/ucdavis-noyce/YouTube-Driver",
+    download_url="",
     description="Programmatically interact with YouTube's interface.",
+    long_description="README.md",
+    package_dir={"": "src"},
     install_requires=[
         'selenium',
         'pyvirtualdisplay'
