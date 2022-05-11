fcbba4a85a19319a82f02518390b7a3b2913c337
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-10 19:56:31 -0700

    version update

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="YouTube-Driver",
-    version="1.0.2",
+    version="1.0.3",
     packages=["ytdriver"],
     license="MIT",
     author="Muhammad Haroon",
     author_email="mharoon@ucdavis.edu",
     url="https://github.com/ucdavis-noyce/YouTube-Driver",
-    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.2.tar.gz",
+    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.3.tar.gz",
     description="Programmatically interact with YouTube's interface.",
     long_description=long_description,
     long_description_content_type='text/markdown',
