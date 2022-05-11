5d25847704c9a72acd42f54f59b378c26b7ed21c
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-10 21:43:09 -0700

    updated example

--- src/ytdriver/example.py
+++ src/ytdriver/example.py
@@ -1,22 +1,24 @@
 from ytdriver import YTDriver
 
-# initialize the driver
-driver = YTDriver(browser='firefox', verbose=True)
 
-# get videos from the youtube homepage
-videos = driver.get_homepage()
+if __name__ == '__main__':
+  # initialize the driver
+  driver = YTDriver(browser='firefox', verbose=True)
 
-# play the top video from the homepage for 30 seconds
-driver.play(videos[0], 30)
+  # get videos from the youtube homepage
+  videos = driver.get_homepage()
 
-# get up-next video recommendations
-for video in driver.get_recommendations():
-  metadata = video.get_metadata()
-  print(metadata['title'])
+  # play the top video from the homepage for 30 seconds
+  driver.play(videos[0], 30)
 
-# search for a keyword
-for video in driver.search_videos('sports'):
-  print(video.url)
-  
-# close driver
-driver.close()
+  # get up-next video recommendations
+  for video in driver.get_recommendations():
+    metadata = video.get_metadata()
+    print(metadata['title'])
+
+  # search for a keyword
+  for video in driver.search_videos('sports'):
+    print(video.url)
+    
+  # close driver
+  driver.close()
