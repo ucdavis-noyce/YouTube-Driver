0205b6a22de0573ae89ac6eb9b853c31f3bbf07d
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-10 20:22:35 -0700

    fixed versioning for major release

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="YouTube-Driver",
-    version="1.0.4",
+    version="1.0.5",
     packages=["ytdriver"],
     license="MIT",
     author="Muhammad Haroon",
     author_email="mharoon@ucdavis.edu",
     url="https://github.com/ucdavis-noyce/YouTube-Driver",
-    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.4.tar.gz",
+    download_url="https://github.com/ucdavis-noyce/YouTube-Driver/archive/refs/tags/v1.0.5.tar.gz",
     description="Programmatically interact with YouTube's interface.",
     long_description=long_description,
     long_description_content_type='text/markdown',
